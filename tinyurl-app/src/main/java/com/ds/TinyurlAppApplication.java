2b6dccbd13ffa01d99f8fac20af55baf2946bdf8
DELL <DELL@DESKTOP-SCDRKCG>
2024-04-17 14:02:57 +0530

    first commit

---
+++ tinyurl-app/src/main/java/com/ds/TinyurlAppApplication.java
@@ -0,0 +1,13 @@
+package com.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyurlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyurlAppApplication.class, args);
+	}
+
+}
